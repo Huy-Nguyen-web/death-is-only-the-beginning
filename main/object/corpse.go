ed18d9af7806d31eb5e9e2fac9827f26b8c9279f
PhanDacSon <phandacson213@gmail.com>
2022-05-25 08:59:11 +0700

    Adding pushing object to player

--- main/object/corpse.go
+++ main/object/corpse.go
@@ -28,6 +28,7 @@ embedded_components {
   "restitution: 0.5\n"
   "group: \"corpse\"\n"
   "mask: \"floor\"\n"
+  "mask: \"player\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
